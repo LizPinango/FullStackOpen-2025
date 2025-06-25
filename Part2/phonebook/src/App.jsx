a37d73e2f5b40b87489db9eea0febfbf80b515d7
LizPinango <108237515+LizPinango@users.noreply.github.com>
2025-06-25 15:05:28 -0400

    Part2: The Phonebook - Step 3 Add input for numbers

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -4,12 +4,15 @@ const App = () => {
   const [persons, setPersons] = useState([
     { 
       id: 1,
-      name: 'Arto Hellas' 
+      name: 'Arto Hellas', 
+      number: '123-1234567'
     }
   ]) 
-  const [newName, setNewName] = useState('')
 
-  const addName = (event) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
     event.preventDefault()
     const nameExists = persons.find(p => p.name === newName)   
     
@@ -18,33 +21,38 @@ const App = () => {
     } else {      
       const newPerson = {
         id: Math.floor(Math.random() * 1000),
-        name: newName      
+        name: newName,
+        number: newNumber      
       }  
       setPersons(persons.concat(newPerson))      
     }    
 
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           <label htmlFor='name-input'>Name:</label>
           <input id='name-input' value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <label htmlFor='number-input'>Number:</label>
+          <input id='number-input' value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.id}>{p.name}</p> )}      
+      {persons.map(p => <p key={p.id}>{p.name} {p.number}</p> )}      
     </div>
   )
 }
