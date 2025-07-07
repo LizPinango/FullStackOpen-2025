c738677373e095f16c37308fa2e82c2eb8a201cd
Liz <108237515+LizPinango@users.noreply.github.com>
2025-07-06 20:00:30 -0400

    Part3: Phonebook database - Step 6 Update the endpoint for fetching one person to conect to database. Update the "/info" endpoint to use the database.

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const Person = require('./models/persons');
 
@@ -14,40 +13,14 @@ app.use(express.json());
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Phonebook</h1>');
-});
-
 app.get('/info', (req, res) => {
   const date = new Date();
-  const info = `<p>Phonebook has info for ${persons.length} people</p>
-                <p>${date}</p>`; 
-  res.send(info);
+  Person.countDocuments({}).then(count => {
+    const info = `<p>Phonebook has info for ${count} people</p>
+      <p>${date}</p>`;
+    res.send(info);
+  })
 });
-
 
 app.get('/api/persons', (req, res) => {
   Person.find({})
@@ -57,13 +30,15 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;   
-  const person = persons.find(p => p.id === id);
-  if (person) {   
-    res.json(person);
-  } else {
-    res.status(404).send({ error: 'Person not found' });
-  } 
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end() 
+      }
+    })
+    .catch(error => next(error))
 });
 
 app.post('/api/persons', (req, res, next) => {
