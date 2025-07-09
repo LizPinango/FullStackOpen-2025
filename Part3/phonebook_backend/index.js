df250063462f46f2b93d9f0af25b08df226e5408
Liz <108237515+LizPinango@users.noreply.github.com>
2025-07-09 16:16:47 -0400

    Part3: Deploying the database backend to production Create a new production build of the frontend.

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors')
 const morgan = require('morgan');
 const Person = require('./models/persons');
 
 const app = express();
-app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
 
