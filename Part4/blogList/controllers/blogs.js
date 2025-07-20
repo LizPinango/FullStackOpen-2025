52f8c4876dfe2e07d3c6d07370ae3dd0eae37754
Liz <108237515+LizPinango@users.noreply.github.com>
2025-07-20 16:17:24 -0400

    Part4: Blog List Expansion - Step 5 Expand blogs so that each blog contains information on the creator of the blog. Expand users so that each user contains information of the blogs created.

--- Part4/blogList/controllers/blogs.js
+++ Part4/blogList/controllers/blogs.js
@@ -1,15 +1,20 @@
 const blogsRouter = require('express').Router();
-const { request, response } = require('../app');
 const Blog = require('../models/blog');
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
   try {
-    const blog = await Blog.findById(request.params.id)
+    const blog = await Blog
+      .findById(request.params.id)
+      .populate('user', { username: 1, name: 1 })
+
     if (blog) {
       response.json(blog)
     } else {
@@ -23,15 +28,24 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   try {
-    const savedBlog = await blog.save()
+    const savedBlog = await blog.save()    
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   } catch (exception) {
     next(exception)
