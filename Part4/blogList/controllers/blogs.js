822309cfeaac289afa93d511564a6f61356f6d40
Liz <108237515+LizPinango@users.noreply.github.com>
2025-07-20 17:17:45 -0400

    Part4: Blog List Expansion- step 7 Refactor Post request to api/blogs to only work if a valid token is sent.

--- Part4/blogList/controllers/blogs.js
+++ Part4/blogList/controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog');
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -29,7 +38,12 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
