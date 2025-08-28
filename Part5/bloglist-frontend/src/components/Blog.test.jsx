695a8851b9dbbfeeafb14f4158b1af00a0612883
Liz <108237515+LizPinango@users.noreply.github.com>
2025-08-28 10:00:21 -0400

    Part5: Blog List Tests - step 3 Create test for the like button in the Blog component

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
 import Blog from "./Blog";
 
@@ -30,5 +30,21 @@ describe('Blog', () => {
 
     expect(screen.getByText('http://test.com', { exact: false })).toBeDefined()
     expect(screen.getByText('likes 1', { exact: false })).toBeDefined()
+  })
+
+  test("clicking like twice calls event handler twice", async () => {
+    const mockLikeHandler = vi.fn()
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} likeBlog={mockLikeHandler} />)
+
+    const buttonShow = screen.getByText('show more')
+    await user.click(buttonShow)
+
+    const buttonLike = screen.getByText('like')
+    await user.click(buttonLike)
+    await user.click(buttonLike)
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
   })
 })
