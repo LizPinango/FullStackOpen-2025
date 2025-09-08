b3890b4482225aeca2a7a00458d5bd79ecce2c6c
Liz <108237515+LizPinango@users.noreply.github.com>
2025-09-08 15:38:58 -0400

    Part5: Blog List End To End Testing - Step 2 Create test for login with valid and invalid credentials. Create new path in backend to reset the database.

--- Part5/Playwright-E2E-tests/tests/bloglist.spec.js
+++ Part5/Playwright-E2E-tests/tests/bloglist.spec.js
@@ -1,12 +1,40 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Pedro Perez',
+        username: 'user1',
+        password: 'pass123'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('user1')
+      await page.getByRole('textbox').last().fill('pass123')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Pedro Perez logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('user1')
+      await page.getByRole('textbox').last().fill('nopass')
+      await page.getByRole('button', { name: 'login' }).click()
+      
+      const errorDiv = page.locator('.error-box')
+      await expect(errorDiv).toContainText('Wrong credentials')
+    })
   })
 })
