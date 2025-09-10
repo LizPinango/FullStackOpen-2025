9e49c1b905f47d8c59b256d470821d63883bc1b0
Liz <108237515+LizPinango@users.noreply.github.com>
2025-09-10 14:57:10 -0400

    Part5: Blog List E2E Testing (Cypress) - Step 2 Make tests for logging in with invalid and valid credentials Add id to the login button

--- Part5/Cypress-E2E-tests/cypress/e2e/bloglist.cy.js
+++ Part5/Cypress-E2E-tests/cypress/e2e/bloglist.cy.js
@@ -1,10 +1,35 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Pedro Perez',
+      username: 'user1',
+      password: 'pass123'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user) 
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type('user1')
+      cy.get('input:last').type('pass123')
+      cy.get('#login-btn').click()
+
+      cy.contains('Pedro Perez logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('user1')
+      cy.get('input:last').type('nopass')
+      cy.get('#login-btn').click()
+
+      cy.get('.error-box').contains('Wrong credentials')
+    })
   })
 })
