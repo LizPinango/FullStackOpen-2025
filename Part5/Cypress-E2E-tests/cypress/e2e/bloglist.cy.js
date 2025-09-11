78bf438942c4b7a2a88395678533d07e8bddea96
Liz <108237515+LizPinango@users.noreply.github.com>
2025-09-11 10:18:40 -0400

    Part5: Blog List E2E Testing (cypress) - Step 6 Make a test ensuring that only the creator can see the delete button of a blog.

--- Part5/Cypress-E2E-tests/cypress/e2e/bloglist.cy.js
+++ Part5/Cypress-E2E-tests/cypress/e2e/bloglist.cy.js
@@ -1,12 +1,16 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    const user = {
+    cy.request('POST', 'http://localhost:3001/api/users/', {
       name: 'Pedro Perez',
       username: 'user1',
       password: 'pass123'
-    }
-    cy.request('POST', 'http://localhost:3001/api/users/', user) 
+    }) 
+    cy.request('POST', 'http://localhost:3001/api/users/', {
+      name: 'Maria Gomez',
+      username: 'user2',
+      password: 'clave456'
+    }) 
     cy.visit('http://localhost:5173')
   })
 
@@ -53,11 +57,18 @@ describe('Blog app', function() {
 
     describe('and a blog exist', function() {
       beforeEach(function() {
+        // 1. Intercept the POST petition and give it an alias
+        cy.intercept('POST', '/api/blogs').as('createBlog');
+
+        // 2. Execute the action that tirggers the POST petition
         cy.contains('button', 'New Blog').click()
         cy.contains('label', 'Title').type('Test Blog')
         cy.contains('label', 'Author').type('Test Author')
-        cy.contains('label', 'Url').type('Test Url')
+        cy.contains('label', 'Url').type('Test Url')        
         cy.get('#add-blog-btn').click()
+
+        // 3. Wait for the petition to complete 
+        cy.wait('@createBlog');
       })
 
       it('a blog can be liked', function() {
@@ -73,6 +84,17 @@ describe('Blog app', function() {
         
         cy.get('.notification-box').contains("'Test Blog' by Test Author deleted")
         cy.get('html').should('not.contain', 'Test Blog - Test Author')
+      })
+
+      it('it can not be deleted by other users', function() {
+        cy.contains('button', 'Logout').click()
+
+        cy.get('input:first').type('user2')
+        cy.get('input:last').type('clave456')
+        cy.get('#login-btn').click()
+
+        cy.contains('button', 'show more').click()
+        cy.contains('delete').should('not.exist') 
       })
     })
   })
