6808bce1cc4fc9c1eff2a757cc756d8fcbb005d9
Liz <108237515+LizPinango@users.noreply.github.com>
2025-11-13 10:42:15 -0400

    Part6: Anecdotes - Step 1 Implement the functionality for voting anecdotes

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,26 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      { 
+        const id = action.playload.id
+        return state.map(anecdote =>
+          anecdote.id !== id
+            ? anecdote
+            : { ...anecdote, votes: anecdote.votes + 1 }
+        ) 
+      }
+    default:
+      return state
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    playload: { id }
+  }
 }
 
 export default reducer
