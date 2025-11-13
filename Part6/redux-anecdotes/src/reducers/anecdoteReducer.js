68ac3253fd62709dc3a338f9bee3dfe8a6a76ca0
Liz <108237515+LizPinango@users.noreply.github.com>
2025-11-13 15:41:26 -0400

    Part6: Anecdotes - step 9 Change the definition of the anecdote reducer and action creators to use the Redux Toolkit's createSlice function.

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,46 +21,28 @@ const asObject = anecdote => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch (action.type) {
-    case 'VOTE':
-      { 
-        const id = action.playload.id
-        return state.map(anecdote =>
-          anecdote.id !== id
-            ? anecdote
-            : { ...anecdote, votes: anecdote.votes + 1 }
-        ) 
-      }
-    case 'CREATE':
-      {
-        const newAnecdote = action.playload
-        return [...state, newAnecdote]
-      }
-    default:
-      return state
-  }
-}
-
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    playload: { id }
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'CREATE',
-    playload: { 
-      content,
-      id: getId(),
-      votes: 0 
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    voteAnecdote(state, action) {
+      const id = action.payload
+      return state.map(anecdote =>
+        anecdote.id !== id
+          ? anecdote
+          : { ...anecdote, votes: anecdote.votes + 1 }
+      )
+    },
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push({ 
+        content,
+        id: getId(),
+        votes: 0 
+      })
     }
-  }
-}
+  }  
+})
 
-export default reducer
+export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
